a9ba0ce592cf6bbd9320839154a4a521c1b05eb1
Ludovic Doppler <ldoppler@k2r4p5.42mulhouse.fr>
2024-03-11 11:57:35 +0100

    in progress

--- main.c
+++ main.c
@@ -6,22 +6,37 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 11:17:06 by ludovicdopp       #+#    #+#             */
-/*   Updated: 2024/03/11 11:34:27 by ldoppler         ###   ########.fr       */
+/*   Updated: 2024/03/11 11:57:08 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int argc, char **argv)
+int	init_philo(t_tab **tab, char **argv)
 {
-    if (argc == 6)
-    {
-        int number_
-        printf("OK\n");
-    }
-    else
-    {
-        printf("Look like there is not enough arguments!\n");
-    }
+	*tab = malloc(sizeof(t_tab));
+	(*tab)->fork = ft_atoi(argv[1]);
+	if ((*tab)->fork < 1)
+	{
+		printf("Not enought philosophers!\n");
+		return (free(*tab), 1);
+	}
+	printf("OK\n");
     return (0);
+}
+int	main(int argc, char **argv)
+{
+	t_tab	*tab;
+
+	if (argc == 6)
+	{
+        if(init_philo(&tab, argv))
+            return (1);
+	}
+	else
+	{
+		printf("Look like there is not enough arguments!\n");
+	}
+	free(tab);
+	return (0);
 }
--- philo.h
+++ philo.h
@@ -1,36 +1,36 @@
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <pthread.h>
-# include <unistd.h>
 # include <time.h>
+# include <unistd.h>
 
 typedef struct s_rules
 {
-    int time_to_die;
-    int time_to_eat;
-    int time_to_sleep;
-    int nbre_philo;
-}t_rules;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nbre_philo;
+}					t_rules;
 
 typedef struct s_tab
 {
-    pthread_mutex_t **mutex;
-    int             fork;
-};
+	pthread_mutex_t	**mutex;
+	int				fork;
+}					t_tab;
 
 typedef struct t_philo
 {
-    int id;
-    int cur_die;
-    int cur_eat;
-    int cur_sleep;
-} t_philo;
+	int				id;
+	int				cur_die;
+	int				cur_eat;
+	int				cur_sleep;
+}					t_philo;
 
 /*utils function*/
-int	ft_atoi(const char *str);
+int					ft_atoi(const char *str);
 
 /*philo function*/
 

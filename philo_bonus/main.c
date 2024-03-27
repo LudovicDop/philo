a90bafc8d3ab578722e8766a33818c2a6516b3cf
Ludovic Doppler <ldoppler@k2r4p2.42mulhouse.fr>
2024-03-27 11:15:25 +0100

    in progress (bonus)

--- philo_bonus/main.c
+++ philo_bonus/main.c
@@ -6,24 +6,12 @@
 /*   By: ldoppler <ldoppler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 12:30:45 by ludovicdopp       #+#    #+#             */
-/*   Updated: 2024/03/27 11:13:44 by ldoppler         ###   ########.fr       */
+/*   Updated: 2024/03/27 11:15:16 by ldoppler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_bonus.h"
-#define NBRE_PHILO 4
 
-// void    *routine(void *arg)
-// {
-//     t_philo *philo;
-
-//     philo = arg;
-//     sem_wait(&philo->sem);
-//     sleep(1);
-//     printf("Number : %d\n", philo->id);
-//     sem_post(&philo->sem);
-//     return (NULL);
-// }
 void    *philosophers(void *arg)
 {
     t_philo *philo;
@@ -37,19 +25,18 @@ void    start_thread(t_philo *philo)
     int i;
 
     i = 0;
-    printf("nbre : %d\n", philo->rules->nbre_of_philo);
-    // while (i < philo->rules->nbre_of_philo)
-    // {
-    //     philo[i].id = i + 1;
-    //     pthread_create(&philo[i].th, NULL, &philosophers, &philo[i]);
-    //     i++;
-    // }
-    // i = 0;
-    // while (i < 5)
-    // {
-    //     pthread_join(philo[i].th, NULL);
-    //     i++;
-    // } 
+    while (i < philo->rules->nbre_of_philo)
+    {
+        philo[i].id = i + 1;
+        pthread_create(&philo[i].th, NULL, &philosophers, &philo[i]);
+        i++;
+    }
+    i = 0;
+    while (i < 5)
+    {
+        pthread_join(philo[i].th, NULL);
+        i++;
+    } 
 }
 int main(int argc, char **argv)
 {
